113014f190ea8fe54803ecb0b17162c872313724
n-kawamoto <n-kawamoto@system-ties.co.jp>
2015-06-07 16:40:16 +0900

    登録処理、画面の作成

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -4,12 +4,25 @@ class UserController < ApplicationController
   end
 
   def new
+    @user = User.new
   end
 
   def edit
   end
 
   def create
+    user_params = params.require(:user).permit(
+      :mail,
+      :name,
+      :password,
+      :password_confirmation
+    )
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to login_path
+    else
+      render :new
+    end
   end
 
   def update
