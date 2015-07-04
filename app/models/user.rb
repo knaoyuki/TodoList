69aa177c638225baf54b99b24bcc5ac48b834a6c
n-kawamoto <n-kawamoto@system-ties.co.jp>
2015-07-04 15:13:56 +0900

    登録画面チェック処理

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
   authenticates_with_sorcery!
-
-  validates :password, confirmation: true
+  EMAILCHECK = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :mail, presence: true, length: {maximum: 60}, format: {with: EMAILCHECK }
+  validates :name, presence: true, length: {maximum: 60}
+  validates :password, confirmation: true, presence: true, length: {minimum: 8}
 end
