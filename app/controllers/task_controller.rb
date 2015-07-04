b26923c4df2763ec7613f06d94092240782899b1
n-kawamoto <n-kawamoto@system-ties.co.jp>
2015-07-04 11:28:33 +0900

    タスク登録処理追加

--- app/controllers/task_controller.rb
+++ app/controllers/task_controller.rb
@@ -5,6 +5,19 @@ class TaskController < ApplicationController
   end
 
   def create
+    task_params = params[:task]
+    @task = Task.new
+    @task.user = User.find(task_params[:user])
+    @task.completed = false
+    @task.priority = Priority.find(task_params[:priority])
+    @task.description = task_params[:description]
+    @task.deleted = false
+    if @task.save
+      redirect_to :tasks
+    else
+      @tasks = Task.all
+      render :index
+    end
   end
 
   def update
